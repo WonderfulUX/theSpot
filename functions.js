2ec053a6c56981ab3e00618ce0a7b171c126c9a1
Boris Boulogne <bboulogne@allmol.com>
2021-06-17 12:27:39 -0400

    Withdrawal em values

--- functions.js
+++ functions.js
@@ -1,13 +1,30 @@
 const menu = document.getElementById('NavTextLinksContainer');
 const button = document.getElementById('MobileMenuButton');
-const menuLines = document.querySelectorAll('.menuLine')
+const menuLines = document.querySelectorAll('.menuLine');
+let menuDisplayed = false;
 
 button.addEventListener('click', ()=>{
-  if(menu.classList.contains('displayed')){
-      menu.classList.remove('displayed');
-  }
-  else{
-    menu.classList.add('displayed');
-  }
+    menu.classList.toggle('displayed');
+    //toggleMenu();
 })
 
+// function toggleMenu(){
+//     if(menuLines[0].style.margin!="0px"){
+//         console.log('IF');
+//         menuLines[0].style.margin="0";
+//         menuLines[1].style.margin="0";
+//         menuLines[1].style.opacity="0";
+//         menuLines[2].style.margin="0";
+//         menuLines[0].style.transform="translateY(50%) rotate(45deg)";
+//         menuLines[2].style.transform="translateY(-50%) rotate(-45deg)";
+//     }
+//     else{
+//         console.log('ELSE');
+//         menuLines[0].style.margin="3px 0px";
+//         menuLines[0].style.margin="3px 0px";
+//         menuLines[1].style.opacity="1";
+//         menuLines[2].style.margin="3px 0px";
+//         menuLines[0].style.transform="translateY(-50%) rotate(0deg)";
+//         menuLines[2].style.transform="translateY(50%) rotate(0deg)";
+//     }
+// }
