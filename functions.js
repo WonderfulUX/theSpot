fac17c468ecdf53ea74c1fad3f6833e78d47f2e3
Boris Boulogne <bboulogne@allmol.com>
2021-06-21 11:52:09 -0400

    Scrollbar added for big Event titles

--- functions.js
+++ functions.js
@@ -5,26 +5,4 @@ let menuDisplayed = false;
 
 button.addEventListener('click', ()=>{
     menu.classList.toggle('displayed');
-    //toggleMenu();
 })
-
-// function toggleMenu(){
-//     if(menuLines[0].style.margin!="0px"){
-//         console.log('IF');
-//         menuLines[0].style.margin="0";
-//         menuLines[1].style.margin="0";
-//         menuLines[1].style.opacity="0";
-//         menuLines[2].style.margin="0";
-//         menuLines[0].style.transform="translateY(50%) rotate(45deg)";
-//         menuLines[2].style.transform="translateY(-50%) rotate(-45deg)";
-//     }
-//     else{
-//         console.log('ELSE');
-//         menuLines[0].style.margin="3px 0px";
-//         menuLines[0].style.margin="3px 0px";
-//         menuLines[1].style.opacity="1";
-//         menuLines[2].style.margin="3px 0px";
-//         menuLines[0].style.transform="translateY(-50%) rotate(0deg)";
-//         menuLines[2].style.transform="translateY(50%) rotate(0deg)";
-//     }
-// }
