2dcde848ee4c84b15453f5aa126f2e565b955fad
Boris Boulogne <bboulogne@allmol.com>
2021-07-01 11:36:59 -0400

    modale almost done

--- functions.js
+++ functions.js
@@ -1,8 +1,37 @@
-const menu = document.getElementById('NavTextLinksContainer');
+const mobileMenu = document.getElementById('NavTextLinksContainer');
 const button = document.getElementById('MobileMenuButton');
 const menuLines = document.querySelectorAll('.menuLine');
 let menuDisplayed = false;
+const closeModaleButtons = document.querySelectorAll('.close');
+const contactNavButton = document.querySelectorAll('.contact');
+const modaleContact = document.getElementById('InfoContactModale');
 
 button.addEventListener('click', ()=>{
-    menu.classList.toggle('displayed');
+    mobileMenu.classList.toggle('displayed');
 })
+
+
+for(i=0; i<contactNavButton.length; i++){
+    contactNavButton[i].addEventListener('click',displayModale);
+}
+for(i=0; i<closeModaleButtons.length; i++){
+    closeModaleButtons[i].addEventListener('click',closeModale);
+}
+
+
+
+
+function displayModale(){
+    modaleContact.classList.add("modaleDisplay");
+    setTimeout(()=>{
+        modaleContact.querySelector(".modaleBox").classList.add("slide");
+    },100)
+}
+
+function closeModale(e){
+    console.log(e.target);
+    e.target.parentElement.parentElement.classList.remove("modaleDisplay");
+    setTimeout(()=>{
+        e.target.parentElement.classList.remove("slide");
+    },300)
+}
