7c60d2441c6486d1d8ae46d1a93042bb9bb64cb9
Boris Boulogne <bboulogne@allmol.com>
2021-07-05 11:02:42 -0400

    quantity modale in progress

--- functions.js
+++ functions.js
@@ -1,26 +1,19 @@
-const mobileMenu = document.getElementById('NavTextLinksContainer');
-const button = document.getElementById('MobileMenuButton');
+const mobileMenuBlock = document.getElementById('NavTextLinksContainer');
+const mobileMenuButton = document.getElementById('MobileMenuButton');
 const menuLines = document.querySelectorAll('.menuLine');
-let menuDisplayed = false;
 const closeModaleButtons = document.querySelectorAll('.closeArea');
 const contactNavButton = document.querySelectorAll('.contact');
 const modaleContact = document.getElementById('InfoContactModale');
+const minus = document.getElementById('MinusButton');
+const plus = document.getElementById('PlusButton');
+const quantity = document.querySelector('.quantityValue');
 
-button.addEventListener('click', ()=>{
-    mobileMenu.classList.toggle('displayed');
+
+//MOBILE MENU
+mobileMenuButton.addEventListener('click', ()=>{
+    mobileMenuBlock.classList.toggle('displayed');
 })
-
-
-for(i=0; i<contactNavButton.length; i++){
-    contactNavButton[i].addEventListener('click',displayMenu);
-}
-for(i=0; i<closeModaleButtons.length; i++){
-    closeModaleButtons[i].addEventListener('click',closeModale);
-}
-
-
-
-
+//---------------------------------------
 function displayMenu(){
     modaleContact.classList.add("modaleDisplay");
     setTimeout(()=>{
@@ -36,20 +29,22 @@ function closeModale(e){
 }
 
 
-// //CALENDAR
-// fetch("https://world-clock.p.rapidapi.com/jsonp/cet/utc?callback=callback", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "a0c10f9e8fmsh43cf769a4c9cf9bp1faa7cjsn20435422cd83",
-// 		"x-rapidapi-host": "world-clock.p.rapidapi.com"
-// 	}
-// })
-// .then(response=> {
-// 	console.log(response.json());
-// })
-// // .then(data=>{
-// //     console.log(data);
-// // })
-// .catch(err => {
-// 	console.error(err);
-// });
+// MODALE CONTACT
+for(i=0; i<contactNavButton.length; i++){
+    contactNavButton[i].addEventListener('click',displayMenu);
+}
+for(i=0; i<closeModaleButtons.length; i++){
+    closeModaleButtons[i].addEventListener('click',closeModale);
+}
+
+
+
+// MODALE QUANTITY
+minus.addEventListener('click',()=>{
+    if(quantity.innerText!=0){
+        quantity.innerText = +quantity.innerText-1;
+    }
+})
+plus.addEventListener('click',()=>{
+    quantity.innerText = +quantity.innerText+1;
+})
