1692047751a0e08d232f2a0fcd1c302f0146862a
Boris Boulogne <bboulogne@allmol.com>
2021-07-06 07:45:12 -0400

    Can't update quantity Indicator, Modale Quantity ok and responsive

--- functions.js
+++ functions.js
@@ -1,39 +1,50 @@
-const mobileMenuBlock = document.getElementById('NavTextLinksContainer');
-const mobileMenuButton = document.getElementById('MobileMenuButton');
-const menuLines = document.querySelectorAll('.menuLine');
-const closeModaleButtons = document.querySelectorAll('.closeArea');
-const contactNavButton = document.querySelectorAll('.contact');
-const modaleContact = document.getElementById('InfoContactModale');
-const minus = document.getElementById('MinusButton');
-const plus = document.getElementById('PlusButton');
-const quantity = document.querySelector('.quantityValue');
 
+const mobileMenuBlock = document.getElementById('NavTextLinksContainer'),
+      mobileMenuButton = document.getElementById('MobileMenuButton'),
+      menuLines = document.querySelectorAll('.menuLine'),
+      closeModaleButtons = document.querySelectorAll('.close'),
+      contactNavButton = document.querySelectorAll('.contact'),
+      modaleContact = document.getElementById('InfoContactModale'),
+      minus = document.getElementById('MinusButton'),
+      plus = document.getElementById('PlusButton'),
+      productList = document.querySelectorAll('.productContainer'),
+      modaleQuantity = document.getElementById('ModaleQuantity'),
+      modaleBox = document.getElementById('closeMenu'),
+      validateButton = document.getElementById('ValidateQuantity');
+
+
+let temporaryValue,
+    temporaryElement,
+    quantityValue = document.querySelector('.quantityValue') ;
 
 //MOBILE MENU
 mobileMenuButton.addEventListener('click', ()=>{
     mobileMenuBlock.classList.toggle('displayed');
 })
 //---------------------------------------
-function displayMenu(){
+function showContact(){
     modaleContact.classList.add("modaleDisplay");
     setTimeout(()=>{
         modaleContact.querySelector(".modaleBox").classList.add("slide");
     },100)
 }
 
-function closeModale(e){
-    e.target.parentElement.parentElement.classList.remove("modaleDisplay");
+function closeModale(){
+    this.parentElement.parentElement.classList.remove("modaleDisplay");
     setTimeout(()=>{
-        e.target.parentElement.classList.remove("slide");
-    },300)
+        this.parentElement.classList.remove("slide");
+        if(this.parentElement.classList.contains('modaleBox')){
+            quantityValue.innerText='1';
+        }
+    },300);
 }
 
 
 // MODALE CONTACT
-for(i=0; i<contactNavButton.length; i++){
-    contactNavButton[i].addEventListener('click',displayMenu);
+for(let i=0; i<contactNavButton.length; i++){
+    contactNavButton[i].addEventListener('click',showContact);
 }
-for(i=0; i<closeModaleButtons.length; i++){
+for(let i=0; i<closeModaleButtons.length; i++){
     closeModaleButtons[i].addEventListener('click',closeModale);
 }
 
@@ -41,10 +52,51 @@ for(i=0; i<closeModaleButtons.length; i++){
 
 // MODALE QUANTITY
 minus.addEventListener('click',()=>{
-    if(quantity.innerText!=0){
-        quantity.innerText = +quantity.innerText-1;
+    if(quantityValue.innerText!=0){
+        quantityValue.innerText = +quantityValue.innerText-1;
+        temporaryValue = +quantityValue.innerTex;
     }
 })
 plus.addEventListener('click',()=>{
-    quantity.innerText = +quantity.innerText+1;
+    quantityValue.innerText = +quantityValue.innerText+1;
+    temporaryValue = +quantityValue.innerText;
 })
+
+
+for (product of productList){
+        product.addEventListener('click',()=>{
+            modaleQuantity.classList.add('modaleDisplay');
+        });
+    }
+
+
+// for (product of productList){
+//     product.addEventListener('click',changeQuantity);
+// }
+
+
+// function changeQuantity(e){
+//     console.log(e.target);
+//     elementSelection(e.target);
+//     updateQuantity();
+// }
+
+// function elementSelection(){
+//     console.log('selection');
+//     console.log(this);
+//     temporaryElement = product;
+//     quantityValue = product.querySelector('.quantityIndicator').innerText;
+//     console.log(temporaryElement);
+//     console.log(quantityValue);
+// }
+
+// function updateQuantity(){
+//     console.log('update');
+//     modaleQuantity.classList.add('modaleDisplay');
+//     validateButton.addEventListener('click',(element)=>{
+//         console.log('click');
+//         element.innerText = temporaryValue;
+//         console.log(element);
+//     });
+// }
+
